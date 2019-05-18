a9631d7befb8b8fef4961128b974b7e7a7660a32
Miguel Angel <sharky310@gmail.com>
2019-05-18 18:31:41 +0200

    Increase config in gitignore

--- database/psql-pool.js
+++ database/psql-pool.js
@@ -2,13 +2,21 @@
 
 const psql = require('pg').Pool;
   
-const  pool =  new psql({
-    user: process.env.POSTGRESQL_USER,
-    host: process.env.POSTGRESQL_HOST,
-    database: process.env.POSTGRESQL_DATABASE,
-    password: process.env.POSTGRESQL_PASSWORD,
-    port: process.env.POSTGRESQL_PORT,
-  });
+// const  pool =  new psql({
+//     user: process.env.POSTGRESQL_USER,
+//     host: process.env.POSTGRESQL_HOST,
+//     database: process.env.POSTGRESQL_DATABASE,
+//     password: process.env.POSTGRESQL_PASSWORD,
+//     port: process.env.POSTGRESQL_PORT,
+//   });
+
+const config = {
+  user: process.env.POSTGRESQL_USER,
+      host: process.env.POSTGRESQL_HOST,
+      database: process.env.POSTGRESQL_DATABASE,
+      password: process.env.POSTGRESQL_PASSWORD,
+      port: process.env.POSTGRESQL_PORT,
+}
   
   try {
     if (pool) {

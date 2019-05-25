60b38b6c1b7f6ed7290a6e2bb77359a5839a9711
Miguel Angel <sharky310@gmail.com>
2019-05-25 20:12:01 +0200

    [ADD & REMOVE] Add cloudinary dependency and remove filestack dependency

--- brain/controllers/document/upload-document-controller.js
+++ brain/controllers/document/upload-document-controller.js
@@ -2,14 +2,24 @@
 
 const document = require('../../../database/models/Document');
 
-const client = require('filestack-js').init(process.env.FILESTACK_API_KEY);
+var cloudinary = require('cloudinary');
 
-async function uploadDocumentController(req, res, next) {
 
-    const result = await client.upload("https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjn4eLywrbiAhUQBWMBHaddB34QjRx6BAgBEAU&url=https%3A%2F%2Fwww.theverge.com%2Ftldr%2F2018%2F10%2F29%2F18039884%2Fjames-cameron-avatar-sequels-papyrus-font-logo-rip&psig=AOvVaw24dqIMtfMkIQRM5kbWd0pj&ust=1558868245926314");
-    console.log(result);
-    res.send(404);
+async function uploadDocument(req, res, next) {
+
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARI_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARI_API_KEY, 
+        api_secret: process.env.CLOUDINARI_API_SECRET
+      });
+
+    try{
+        await cloudinary.uploader.upload("../../../internal_docs/documents/onboarding-linkedin.pdf", {public_id: 'single_page_pdf'}, function(result) { })
+        res.status(201);
+    } catch(e){
+        res.status(400).send(e);
+    }
 
 }
 
-module.exports = uploadDocumentController;
+module.exports = uploadDocument;
--- brain/routes/document-router.js
+++ brain/routes/document-router.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const express = require('express');
+const multer = require('multer');
 
+const upload = multer();
 const router = express.Router();
+
+const uploadDocumentController = require('../controllers/document/upload-document-controller');
 
 router.post('/document', uploadDocumentController);
 

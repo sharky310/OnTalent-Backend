a54c5d6b95570f00262dd47833dd1f0f5fe96492
Miguel Angel <sharky310@gmail.com>
2019-06-20 19:45:15 +0200

    [ADD] routes

--- brain/controllers/document/get-documents-controller.js
+++ brain/controllers/document/get-documents-controller.js
@@ -4,21 +4,18 @@ const document = require('../../../database/models/Document');
 
 const stream = require('stream');
 
+/**
+ * Return list of documents
+ */
 async function getDocument(req, res, next){
 
-    document.findOne({ where: {name: req.query.id} }).then(file=> {
-        const fileContents = Buffer.from(file.dataValues.document, "base64");
-        const readStream = new stream.PassThrough();
-        readStream.end(fileContents);
-    
-         res.set('Content-disposition', 'attachment; filename=' + file.name);
-         res.set('Content-Type', file.type);
- 
-         readStream.pipe(res);
-  }).catch(err => {
-    console.log(err);
-    res.json({msg: 'Error', detail: err});
-      })
+  document.findAll({ attributes: ['name'] })
+  .then(document => {
+    res.status(200).send(document)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
 
 module.exports = getDocument;
